c9da9195d69da4eea89fe2bb111a025a382d493a
John <john2baik@gmail.com>
2017-04-07 17:45:50 -0400

    friday 4/7
    gonna make a cluster based on 2 inputs only

--- src/NumericalCluster.java
+++ src/NumericalCluster.java
@@ -1,46 +1,196 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Created by johnbaik on 4/4/17.
  */
 public class NumericalCluster {
     private List <double[]> data;
+    private List<List<double[]>> clusters;
     private String attributeDescriptions;
     private double[] max;
     private double[] min;
-    private double[] oldCenter;
-    private double[] newCenter;
+    private List<double[]> oldCenter;
+    private List<double[]> newCenter;
     private int tupleLength;
     private int dataSize;
+    private int k;
+    private boolean hasChanged;
+    private int numOfAttributes;
+    private final double threshold = 1.0;
+    int attribute1;
+    int attribute2;
 
-    public NumericalCluster(List<double[]> data, String attributeDescriptions, double[] max, double[] min){
+
+
+    public NumericalCluster(List<double[]> data, String attributeDescriptions, double[] max, double[] min, int k, int attribute1, int attribute2){
         this.data = data;
         this.attributeDescriptions = attributeDescriptions;
         this.max = max;
         this.min = min;
         tupleLength = data.get(0).length;
         dataSize = data.size();
+        this.k = k;
+        hasChanged = true;
+        numOfAttributes = data.get(0).length;
+        this.attribute1 = attribute1;
+        this.attribute2 = attribute2;
+        if(attribute1 < 0 || attribute2 < 0){
+            runAll();
+        }
+        else
+            run();
 
-
-        run();
-       // System.out.println(attributeDescriptions);
+        // System.out.println(attributeDescriptions);
     }
 
     private void run(){
+
+    }
+
+    private void runAll(){
         initialSetup();
+        cluster();
+    }
+
+    private void cluster(){
+        while(true){
+            createClusters();
+            assignCluster();
+            updateClusterMean();
+            checkForChange();
+            if(!hasChanged){//clusters is now all clustrized
+                break;
+            }
+            else{
+                updateCenters();
+            }
+        }
+    }
+
+    private void updateCenters(){
+        oldCenter = new ArrayList<>(newCenter);
+    }
+
+    private void checkForChange(){
+        hasChanged = true;//only change when ALL centers have not changed by a 5% margin
+        for(int i = 0; i < k; i++){
+            double[] oldCenterTuple = oldCenter.get(i);
+            double[] newCenterTuple = newCenter.get(i);
+            double difference = Math.abs((oldCenterTuple[i] - newCenterTuple[i]));
+            double percentChange = (difference / oldCenterTuple[i]) * 100.0;
+            //consider all centers together. none or all rule.
+            if(percentChange <= threshold){
+                hasChanged = false;
+            }
+            else if(percentChange > threshold){
+                hasChanged = true;
+            }
+        }
+    }
+
+    private void updateClusterMean(){
+        newCenter = new ArrayList<>();
+        for(List<double[]> cluster : clusters){
+            int clusterSize = cluster.size();
+            double[] sum = new double[numOfAttributes];
+            for(double[] tuple : cluster){
+                for(int i = 0; i < numOfAttributes; i++){
+                    sum[i] += tuple[i];
+                }
+            }
+            double[] average = new double[numOfAttributes];
+            for(int i = 0; i < numOfAttributes; i++){
+                average[i] = sum[i] / clusterSize;
+            }
+            newCenter.add(average);
+        }
+
+    }
+    private void assignCluster() {
+        for (double[] tuple : data) {
+            double minDist = Double.MAX_VALUE;
+            int pos = 0;
+            //System.out.println("Current cluster size : " + centers.size());
+            for (int curCluster = 0; curCluster < clusters.size(); curCluster++) {
+                List<double[]> cluster = clusters.get(curCluster);
+                double[] centroid = cluster.get(0);//this is the centeroid, always at the beginning of the list;
+                double distance = euclideanDistance(centroid, tuple);
+                if (distance < minDist) {
+                    minDist = distance;
+                    pos = curCluster;
+                }
+            }
+            clusters.get(pos).add(tuple);
+        }
+    }
+
+    public double euclideanDistance(double[] centroid, double[] tuple){
+        double total = 0, difference;
+        for(int i = 0; i < numOfAttributes; i++){
+            difference = centroid[i] - tuple[i];
+            total += difference * difference;
+        }
+        return  Math.sqrt(total);
+    }
+
+    private void createClusters(){
+        clusters = new ArrayList<>();
+        for(double[] centroid : oldCenter){
+            List<double[]> tempList = new ArrayList<>();
+            tempList.add(centroid);
+            clusters.add(tempList);
+        }
     }
 
     private void initialSetup(){
-
+        setInitialoldCenter();
     }
 
-    //-----------------initial
+    private void setInitialoldCenter(){
+        oldCenter = new ArrayList<>();
+        for(int i = 0; i < k; i++){
+            int random = (int) (Math.random()*dataSize);
+            oldCenter.add(data.get(random));
+        }
+    }
 
-    public void printData(){
-        for(double[] tuple : data){
+
+    //from normalized data, set k random initial centeroids
+//    public void setInitialPoints(){
+//        newCenter = new ArrayList<double[]>();
+//        for(int i = 0; i < k; i++){
+//            int random = (int) (Math.random() * dataSize);
+//            newCenter.add(normalizedData.get(random));
+//        }
+//    }
+
+    //-----------------initial----------
+
+    public void printData() {
+        for (double[] tuple : data) {
             System.out.println(Arrays.toString(tuple));
         }
+    }
+    public List<List<double[]>> getClusters(){
+        return clusters;
+    }
+
+    public void printClusters(){
+        int count=0;
+        for(List<double[]>cluster: clusters){
+            System.out.println("This is cluster number " + count + ".\n\n");
+            System.out.println("The size of this cluster is " + cluster.size() + ".");
+            for(double[] x : cluster ){
+                System.out.println(Arrays.toString(x));
+            }
+            count++;
+        }
+    }
+
+    public String getAttributeDescriptions(){
+        return attributeDescriptions;
     }
 
 
--- src/NumericalClusterTest.java
+++ src/NumericalClusterTest.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by johnbaik on 4/5/17.
@@ -7,9 +9,34 @@ public class NumericalClusterTest {
     public static void main(String[] args){
         ParserNormalizer pokemon = new ParserNormalizer("/Users/johnbaik/IdeaProjects/Pokemon_data_mining/src/pokemon.csv");
         String attributeDescriptions = pokemon.getAttributeDescriptions();
-        NumericalCluster numCluster = new NumericalCluster(pokemon.getDoubleAttributes(), pokemon.getAttributeDescriptions(), pokemon.getMaxiNumOfAttribs(), pokemon.getMiniNumOfAttribs();
-        numCluster.printData();
+        NumericalCluster numCluster = new NumericalCluster(pokemon.getDoubleAttributes(), pokemon.getAttributeDescriptions(), pokemon.getMaxiNumOfAttribs(), pokemon.getMiniNumOfAttribs(), 5, -1, -1);
+        //numCluster.printData();
+       // System.out.println(numCluster.getAttributeDescriptions());
+      //  numCluster.printClusters();
 
+
+        //now try to do with given 2 attributes
+        int x=0, y=0;
+        Scanner sc = new Scanner(System.in);
+        double[] goodExample = pokemon.getDoubleAttributes().get(0);
+
+        System.out.println("this is the example\n" + Arrays.toString(goodExample));
+        String attributeList = pokemon.getAttributeDescriptions();
+        String[] attributes = attributeList.split(",");
+        int count = 0;
+
+        for(int i = 0; i < attributes.length; i++){
+            if(goodExample[i] > 0){
+                System.out.println(count++ + " : " + attributes[i] );
+
+            }
+        }
+
+        System.out.println("Please input 2 numbers that you would like to see the correlation:\n");
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        NumericalCluster dosAttributes = new NumericalCluster(pokemon.getDoubleAttributes(), pokemon.getAttributeDescriptions(), pokemon.getMaxiNumOfAttribs(), pokemon.getMiniNumOfAttribs(), 5, x, y);
 
     }
 }
